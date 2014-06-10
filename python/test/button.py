5ac3688e678f4c414f05818b53c2026aa04d3069
harrimakela <harri@harrimakela-VB.(none)>
2014-06-10 15:23:14 +0300

    hello world

--- python/test/button.py
+++ python/test/button.py
@@ -1,29 +1,3 @@
 #!/usr/bin/python
 
-import Tkinter as tk
-import time
-import sys
-
-class ExampleApp(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-        self.label = tk.Label(self, text="", width=10)
-        self.label.pack()
-        self.remaining = 0
-        self.countdown(10)
-
-    def countdown(self, remaining = None):
-        if remaining is not None:
-            self.remaining = remaining
-
-        if self.remaining <= 0:
-            self.label.configure(text="time's up!")
-
-        else:
-            self.label.configure(text="%d" % self.remaining)
-            self.remaining = self.remaining - 1
-            self.after(1000, self.countdown)
-
-if __name__ == "__main__":
-    app = ExampleApp()
-    app.mainloop()
+print "Hei How, Hengissa ollaan"
